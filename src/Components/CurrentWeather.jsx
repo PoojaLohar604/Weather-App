485030f4c83ceb95d8a120ec0ca4e88e1c7f5a77
PoojaLohar604 <130908223+PoojaLohar604@users.noreply.github.com>
2024-08-24 15:33:41 +0530

    Update CurrentWeather.jsx

--- src/Components/CurrentWeather.jsx
+++ src/Components/CurrentWeather.jsx
@@ -1,9 +1,12 @@
 // src/components/CurrentWeather.js
 import React from "react";
 
+// Component that displays the current weather information
 const CurrentWeather = ({ weather }) => {
+  // If weather data is not available, return null (do not render anything)
   if (!weather) return null;
 
+  // Destructure relevant properties from the weather data
   const { temp, temp_min, temp_max, humidity } = weather.main;
   const { speed, deg } = weather.wind;
   const { description, icon } = weather.weather[0];
@@ -12,22 +15,31 @@ const CurrentWeather = ({ weather }) => {
     <div className="weather-details">
       <h2>Current Weather</h2>
       <div className="main-details">
-       
+        {/* Display current temperature */}
         <p>Temperature: {temp}°</p>
+
+        {/* Display minimum and maximum temperatures */}
         <p>
           Min: {temp_min}° Max: {temp_max}°
         </p>
+
+        {/* Display humidity level */}
         <p>Humidity: {humidity}%</p>
+
+        {/* Display wind speed and direction */}
         <p>
           Wind: {speed} m/s, {deg}°
         </p>
+
+        {/* Display weather description */}
         <p>{description}</p>
+
+        {/* Display weather icon */}
         <img
           src={`http://openweathermap.org/img/w/${icon}.png`}
           alt={description}
-          style={{width:"50px"}}
+          style={{ width: "50px" }}
         />
-       
       </div>
     </div>
   );
